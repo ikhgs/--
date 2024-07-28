39986a94adcef468ae1366c52606268f3dcb37f9
ikhgs <mathematiques45@gmail.com>
2024-07-28 22:57:58 +0300

    Update 00_00mistral.js

--- scripts/cmds/00_00mistral.js
+++ scripts/cmds/00_00mistral.js
@@ -1,36 +1,37 @@
 const axios = require('axios');
 
 module.exports = {
-	config: {
-		name: "mistral",
-		author: "cliff",//api by hazey
-		version: "1.0.0",
-		countDown: 5,
-		role: 0,
-		category: "Ai",
-		shortDescription: {
-			en: "{p}mistral"
-		}
-	},
-	onStart: async function ({ api, event, args }) {
-		try {
-			if (!args[0]) {
-				return api.sendMessage("Please provide a prompt for Mistral.", event.threadID);
-			}
+  config: {
+    name: "mistral",
+    author: "❤️Bruno❤️",//api by bruno
+    version: "1.0.0",
+    countDown: 5,
+    role: 0,
+    category: "Ai",
+    shortDescription: {
+      en: "{p}mistral"
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    try {
+      if (!args[0]) {
+        return api.sendMessage("Please provide a prompt for Llama.", event.threadID);
+      }
 
-			const prompt = encodeURIComponent(args.join(" "));
-			const apiUrl = `https://code-merge-api-hazeyy01.replit.app/api/mistral/response?prompt=${prompt}`;
+      const prompt = encodeURIComponent(args.join(" "));
+      const apiUrl = `https://api-mistral.vercel.app/?ask=${prompt}`;
 
-			const response = await axios.get(apiUrl);
+      const response = await axios.get(apiUrl);
 
-			if (response.data && response.data.response) {
-				api.sendMessage(response.data.response, event.threadID);
-			} else {
-				api.sendMessage("Unable to get a response from Mistral.", event.threadID);
-			}
-		} catch (error) {
-			console.error('Error making Mistral API request:', error.message);
-			api.sendMessage("An error occurred while processing your request.", event.threadID);
-		}
-	}
+      if (response.data && response.data.response) {
+        api.sendMessage(response.data.response, event.threadID);
+      } else {
+        api.sendMessage("Unable to get a response from Mistral.", event.threadID);
+      }
+    } catch (error) {
+      console.error('Error making Llama API request:', error.message);
+      api.sendMessage("An error occurred while processing your request.", event.threadID);
+    }
+  }
 };
+
